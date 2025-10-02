279c99746d7d2b22fa9828b2567eab590ec8b498
alex <peter.alex.morris@gmail.com>
2025-10-03 05:48:58 +0900

    Added function to add tr and td elements to table with book info
    Function creates tr and td elements with textContent from book object and adds to table

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
-const library = [];
-
-function Book(title, author, pages, read) {
+document.addEventListener("DOMContentLoaded", () => {
+	const table = document.querySelector("#library");
+	function Book(title, author, pages, read) {
 	this.id = crypto.randomUUID();
 	this.title = title;
 	this.author = author;
@@ -8,9 +8,34 @@ function Book(title, author, pages, read) {
 	this.read = read;
 }
 
-function storeBook(title, author, pages, read) {
-	let newBook = new Book(title, author, pages, read);
+	function storeBook(title, author, pages, read) {
+		let newBook = new Book(title, author, pages, read);
 
-	library.push(newBook);
-}
+		library.push(newBook);
+	}
+
+	function showLibrary() {
+		for (let book of library) {
+			const row = document.createElement("tr");
+			row.classList.add(book.id);
+			const title = document.createElement("td");
+			title.textContent = book.title
+			const author = document.createElement("td");
+			author.textContent = book.author
+			const pages = document.createElement("td");
+			pages.textContent = book.pages;
+			const read = document.createElement("td");
+			read.textContent = "false";
+			row.appendChild(title);
+			row.appendChild(author);
+			row.appendChild(pages);
+			row.appendChild(read);
+			table.appendChild(row);
+		}
+	}
+	storeBook("A", "B", "100", true);
+	showLibrary();
+})
+
+const library = [];
 
